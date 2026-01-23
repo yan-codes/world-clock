ac89bb5cad00ded6bbf55f6f73e3ffba44e7730c
Dianne Louise Cardenas <dlpcardenas@gmail.com>
2026-01-23 18:13:04 +0800

    updated current time and date in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
+function updateTime() {
+  let jerusalemElement = document.querySelector("#jerusalem");
+  let jerusalemDateElement = jerusalemElement.querySelector(".date");
+  let jerusalemTimeElement = jerusalemElement.querySelector(".time");
+  jerusalemTime = moment().tz("Asia/Jerusalem");
 
+  jerusalemDateElement.innerHTML = jerusalemTime.format("MMMM Do YYYY");
+  jerusalemTimeElement.innerHTML = jerusalemTime.format(
+    "h:mm:ss [<small>]A[</small>]",
+  );
+
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
